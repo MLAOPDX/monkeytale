72b810c11d97bea53db60f938ba077ef56e56c77
MLAO <21014310+mlao-pdx@users.noreply.github.com>
2022-10-17 12:07:30 -0700

    ci(vulture): Clean-up test trigger

--- monkeytale/file_management.py
+++ monkeytale/file_management.py
@@ -13,8 +13,6 @@ BUILD_DIRECTORY = "build"
 LOG_PATH = SOURCE_PATH / LOG_FILE
 BUILD_PATH = SOURCE_PATH / BUILD_DIRECTORY
 
-# print("Dead code")
-
 
 class MonkeytaleJSONEncoder(EliotJSONEncoder):
     def default(self, o):
